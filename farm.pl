5eb70840a8fd31d6155ec7ff663be15cd24d009c
Carl Masak <cmasak@gmail.com>
2011-07-21 19:53:55 +0200

    rolling two rabbits gives you a rabbit

--- farm.pl
+++ farm.pl
@@ -1,18 +1,25 @@
 class Game {
+    has @.e;
+
     method play_round() {
+        push @.e, {
+            type    => "transfer",
+            from    => "stock",
+            to      => "player 1",
+            animals => { rabbit => 1 },
+        };
     }
 }
 
 use Test;
 
 {
-    my @e;
-    my $game = Game.new(fd => { <rabbit> }, wd => { <rabbit> }, e => @e);
+    my $game = Game.new(fd => { <rabbit> }, wd => { <rabbit> });
     $game.play_round();
-    is_deeply @e, [{
+    is_deeply $game.e, [{
         type    => "transfer",
         from    => "stock",
         to      => "player 1",
         animals => { rabbit => 1 },
-    }];
+    }], "rolling two rabbits gives you a rabbit";
 }
