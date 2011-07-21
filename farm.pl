11c5a78ceb9ac93223d8d32273b552c8271500a5
Carl Masak <cmasak@gmail.com>
2011-07-21 20:08:11 +0200

    rolling if you already have the animal

--- farm.pl
+++ farm.pl
@@ -1,11 +1,13 @@
 class Game {
+    has @.p = {};
     has &!fd;
     has &!wd;
     has @.e;
 
     method play_round() {
         my ($a1, $a2) = &!fd(), &!wd();
-        (my %stock){$_}++ for $a1, $a2;
+        my %stock = @!p[0].list;
+        %stock{$_}++ for $a1, $a2;
         (my %to_transfer){$_} = %stock{$_} div 2
             if %stock{$_} div 2
             for $a1, $a2;
@@ -35,4 +37,16 @@ use Test;
     my $game = Game.new(fd => { <rabbit> }, wd => { <sheep> });
     $game.play_round();
     is_deeply $game.e, [], "rolling rabbit/sheep gives you nothing";
+}
+
+{
+    my $game = Game.new(p => ({ rabbit => 2 }),
+                        fd => { <rabbit> }, wd => { <sheep> });
+    $game.play_round();
+    is_deeply $game.e, [{
+        type    => "transfer",
+        from    => "stock",
+        to      => "player 1",
+        animals => { rabbit => 1 },
+    }], "rolling rabbit/sheep if you already have 2 rabbits";
 }
